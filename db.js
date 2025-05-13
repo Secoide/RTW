7eb1b0fd2b364f2afe2eee0161f470b0d5805156
Guilherme Schvaickardt <gs.gui.gs@hotmail.com>
2025-05-13 09:17:19 -0300

    Novos dados de alteração bd

--- db.js
+++ db.js
@@ -12,13 +12,22 @@ module.exports = connection;
 */
 
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'tramway.proxy.rlwy.net',
-  port: 30181,
-  user: 'root',
-  password: 'QjFuaQovYEfgoITshCKTQRikXmgeMITT',
-  database: 'railway'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao MySQL:', err);
+  } else {
+    console.log('Conectado ao MySQL com sucesso!');
+  }
 });
 
 module.exports = connection;
