b5b53898ceef9292fd8850f111336eae698f7d55
Guilherme Schvaickardt <gs.gui.gs@hotmail.com>
2025-05-13 08:45:53 -0300

    Atualiza credenciais de conexão com Railway

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+/*
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
@@ -5,6 +6,19 @@ const connection = mysql.createConnection({
   user: 'root',
   password: '',
   database: 'bd_cadastro'
+});
+
+module.exports = connection;
+*/
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'tramway.proxy.rlwy.net',
+  port: 30181,
+  user: 'root',
+  password: 'QjFuaQovYEfgoITshCKTQRikXmgeMITT',
+  database: 'railway'
 });
 
 module.exports = connection;
