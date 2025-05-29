38401641d71bc57472073ac41763f51c35ba5bf9
Guilherme Schvaickardt <gs.gui.gs@hotmail.com>
2025-05-29 15:14:30 -0300

    corrigido pool

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -11,6 +11,14 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao MySQL:', err);
+  } else {
+    console.log('Conectado ao MySQL com sucesso!');
+  }
 });
 
 module.exports = connection;
